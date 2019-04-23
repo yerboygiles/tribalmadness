53bb36fab007018a886f0432f0006e07c8fc86b1
GraciousProfishin-alism <thedarkjarl@gmail.com>
2019-04-23 07:48:35 -0700

    abilities for specific stores

--- tt/classes/com/oddlabs/tt/model/Abilities.java
+++ tt/classes/com/oddlabs/tt/model/Abilities.java
@@ -8,21 +8,29 @@ public final strictfp class Abilities {
     // Can attack other units
     public final static int ATTACK = 2;
     // Can harvest resources
-    public final static int HARVEST = 4;
-    // contains supplies
-    public final static int SUPPLY_CONTAINER = 8;
+    public final static int HARVEST = 3;
+    // contains supplies of all kinds
+    public final static int SUPPLY_CONTAINER = 4;
     // builds warriors
-    public final static int BUILD_ARMIES = 16;
+    public final static int BUILD_ARMIES = 5;
     // creates peons
-    public final static int REPRODUCE = 32;
+    public final static int REPRODUCE = 6;
     // Can target other units
-    public final static int TARGET = 64;
+    public final static int TARGET = 7;
     // Can throw weapon
-    public final static int THROW = 128;
+    public final static int THROW = 8;
     // Can be a rally target
-    public final static int RALLY_TO = 256;
+    public final static int RALLY_TO = 9;
     // Can use magic
-    public final static int MAGIC = 512;
+    public final static int MAGIC = 10;
+    // contains wood supplies
+    public final static int SUPPLY_CONTAINER_WOOD = 11;
+    // contains rock supplies
+    public final static int SUPPLY_CONTAINER_ROCK = 12;
+    // contains iron supplies
+    public final static int SUPPLY_CONTAINER_IRON = 13;
+    // contains chicken supplies
+    public final static int SUPPLY_CONTAINER_CHICKEN = 14;
 
     private int abilities;
 
